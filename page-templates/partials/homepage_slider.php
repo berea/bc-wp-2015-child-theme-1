b390bb6c0b5eb2752ef6088106b3a09c2a5ed6e1
Janine Hempy <jhempy@gmail.com>
2015-07-24 16:08:21 -0400

    Icons added to homepage slider.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -8,10 +8,10 @@
 
     <div class="hero-nav-wrapper">
         <ul class="hero-nav">
-            <li>APPLY</li>
-            <li>GIVE</li>
-            <li>VISIT</li>
-            <li>EXPLORE</li>
+            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/pencil.png" width="25" height="25" /><span class="vertical-center-nudge-right">APPLY</span></li>
+            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/heart4.png" width="25" height="25" /><span class="vertical-center-nudge-right">GIVE</span></li>
+            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/library2.png" width="25" height="25" /><span class="vertical-center-nudge-right">VISIT</span></li>
+            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/globe.png" width="25" height="25" /><span class="vertical-center-nudge-right">EXPLORE</span></li>
         </ul>
     </div>
 
