1babfc982db18050c63b358bff3a069b5b38032a
Janine Hempy <jhempy@gmail.com>
2015-07-27 15:08:43 -0400

    Changed icons in hero-nav.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -7,12 +7,24 @@
     <p class="tagline">Donec id elit non mi porta gravida at eget metus. <a href="#">Read More &gt;</a></p>
 
     <div class="hero-nav-wrapper">
-        <ul class="hero-nav">
-            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/pencil.png" width="25" height="25" /><span class="vertical-center-nudge-right">APPLY</span></li>
-            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/heart4.png" width="25" height="25" /><span class="vertical-center-nudge-right">GIVE</span></li>
-            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/library2.png" width="25" height="25" /><span class="vertical-center-nudge-right">VISIT</span></li>
-            <li><img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/globe.png" width="25" height="25" /><span class="vertical-center-nudge-right">EXPLORE</span></li>
-        </ul>
+        <div class="hero-nav">
+            <div class="hero-nav-item">
+                <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apply.png" width="25" height="25" />
+                <span class="vertical-center nudge-right">APPLY</span>
+            </div>
+            <div class="hero-nav-item">
+                <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/give.png" width="25" height="25" />
+                <span class="vertical-center nudge-right">GIVE</span>
+            </div>
+            <div class="hero-nav-item">
+                <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/visit.png" width="25" height="25" />
+                <span class="vertical-center nudge-right">VISIT</span>
+            </div>
+            <div class="hero-nav-item">
+                <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/explore.png" width="25" height="25" />
+                <span class="vertical-center nudge-right">EXPLORE</span>
+            </div>
+        </div>
     </div>
 
     <p>&nbsp;</p>
