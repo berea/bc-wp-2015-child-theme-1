d3eb5ffa69066a71b9684eaf80f950f45c6c50dd
Janine Hempy <jhempy@gmail.com>
2015-08-27 09:44:43 -0400

    Updated secondary_slider to use new without-blogname style.

--- page-templates/partials/secondary_slider.php
+++ page-templates/partials/secondary_slider.php
@@ -1,7 +1,7 @@
 <!-- Child 1 secondary page slider -->
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
     <div class="secondary-slider-background"></div>
-    <div class="slider-textblock">
+    <div class="slider-textblock-without-blogname">
         <div class="slider-breadcrumbs">Bread &nbsp;/&nbsp; Crumbs &nbsp;/&nbsp; Here</div>
         <!-- Blog name removed - not needed in this theme. -->
     </div>
