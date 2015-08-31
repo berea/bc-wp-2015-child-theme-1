a0316a181fbbeb398e9b935a281e6f612a89725d
Charlie C Campbell <hello@charliecampbell.net>
2015-08-31 15:43:54 -0400

    Revision to Text on "Homepage"
    Updated text from previous commit.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -5,7 +5,7 @@
 
     <div class="slider-textblock">
         <h1>THE BEST EDUCATION<br />MONEY CAN'T BUY</h1>
-        <p class="tagline read-more">The New York Times Claims That Colleges Like Berea With a Distinct Focus Have a Hidden Advantage. <a href="http://www.nytimes.com/2014/09/04/upshot/why-colleges-with-a-distinct-focus-have-a-hidden-advantage.html">Read More &gt;</a></p>
+        <p class="tagline read-more">The New York Times writes "Berea College has a hidden advantage."&nbsp; <a href="http://www.nytimes.com/2014/09/04/upshot/why-colleges-with-a-distinct-focus-have-a-hidden-advantage.html">Read More &gt;</a></p>
     </div>
 
     <div class="hero-nav-wrapper">
