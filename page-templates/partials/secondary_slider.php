e0f5b5dee687c4290411d3aca943cb23ec585be9
Janine Hempy <jhempy@gmail.com>
2015-09-04 11:17:52 -0400

    Incremental save so that Skaught can continue my work on the breadcrumbs.

--- page-templates/partials/secondary_slider.php
+++ page-templates/partials/secondary_slider.php
@@ -2,7 +2,9 @@
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
     <div class="secondary-slider-background"></div>
     <div class="slider-textblock-without-blogname">
-        <div class="slider-breadcrumbs">Bread &nbsp;/&nbsp; Crumbs &nbsp;/&nbsp; Here</div>
+        <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+            } ?></div>
         <!-- Blog name removed - not needed in this theme. -->
     </div>
 </div>
