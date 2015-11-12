7b6a98b714e0dd3c6a9442359c87680e6584d2d6
Janine Hempy <jhempy@gmail.com>
2015-11-12 16:07:23 -0500

    Removed images in homepage slider. Charlie will insert images later.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -17,25 +17,25 @@
         <div class="hero-nav">
             <div class="hero-nav-item">
  										<a href="https://www.berea.edu/admissions/applying-for-admission/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apply.png" alt="Apply" width="25" height="25" />
+                    <div class="apply-icon"></div>
                     <span class="vertical-center nudge-right">APPLY</span>
                 </a>
             </div>
             <div class="hero-nav-item">
                 <a href="https://www.berea.edu/give-to-berea/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/give.png" alt="Give" width="25" height="25" />
+                    <div class="give-icon"></div>
                     <span class="vertical-center nudge-right">GIVE</span>
                 </a>
             </div>
             <div class="hero-nav-item">
                 <a href="https://www.berea.edu/admissions/visit-campus/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/visit.png" alt="Visit" width="25" height="25" />
+                    <div class="visit-icon"></div>
                     <span class="vertical-center nudge-right">VISIT</span>
                 </a>
             </div>
             <div class="hero-nav-item">
                 <a href="#">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/explore.png" alt="Explore" width="25" height="25" />
+                    <div class="explore-icon"></div>
                     <span class="vertical-center nudge-right">EXPLORE</span>
                 </a>
             </div>
