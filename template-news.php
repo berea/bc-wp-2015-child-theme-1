9815755f14ff4e3932f38bff1604b252a4b866d6
Janine Hempy <jhempy@gmail.com>
2015-12-07 16:26:04 -0500

    Removed tagging div and template name.

--- template-news.php
+++ template-news.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Template Name: News
- *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
@@ -12,8 +10,6 @@
  */
 
 get_header(); ?>
-
-<div id="news-template"></div>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
