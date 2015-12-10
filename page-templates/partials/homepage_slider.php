2406922df85b7a9388a714c8a8852b62e37df883
Charlie Campbell <hellocharlie@gmail.com>
2015-12-10 10:43:28 -0500

    Updating Navigation
    Navigation target url update for slide "belly band's" Apply link.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -10,7 +10,7 @@
     <div class="hero-nav-wrapper">
         <div class="hero-nav">
             <div class="hero-nav-item">
- 										<a href="http://primary.berea.edu/admissions/applying-for-admission/">
+ 										<a href="http://primary.berea.edu/admissions/apply/">
                     <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apply.png" width="25" height="25" />
                     <span class="vertical-center nudge-right">APPLY</span>
                 </a>
