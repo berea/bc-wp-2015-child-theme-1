4539467201e34b12172d81684846a79a38cd3872
Janine Hempy <jhempy@gmail.com>
2015-12-14 15:13:46 -0500

    Updated "explore" link and removed unnecessary files.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -28,7 +28,7 @@
                 </a>
             </div>
             <div class="hero-nav-item">
-                <a href="#">
+                <a href="https://www.berea.edu/campus">
                     <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/explore.png" width="25" height="25" />
                     <span class="vertical-center nudge-right">EXPLORE</span>
                 </a>
