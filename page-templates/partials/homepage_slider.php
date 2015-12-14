32b608b297dd6e2457eea4cc05258b98e095147c
Janine Hempy <jhempy@gmail.com>
2015-12-14 15:21:45 -0500

    Corrected links from merge madness.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -10,7 +10,7 @@
     <div class="hero-nav-wrapper">
         <div class="hero-nav">
             <div class="hero-nav-item">
- 										<a href="https://primary.berea.edu/admissions/applying-for-admission/">
+                <a href="https://primary.berea.edu/admissions/applying-for-admission/">
                     <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apply.png" width="25" height="25" />
                     <span class="vertical-center nudge-right">APPLY</span>
                 </a>
