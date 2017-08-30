e04efdfb41893ee08e270f96a90ddb82d53435cb
Dan Adams <danadams@Dans-MacBook-Pro-2.local>
2017-08-30 18:12:58 -0400

    adding is_page to secondary_slider

--- page-templates/partials/secondary_slider.php
+++ page-templates/partials/secondary_slider.php
@@ -1,5 +1,18 @@
 <!-- Child 1 secondary page slider -->
+<?php
+if(is_page(970))
+{
+?>
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192_givingday ">
+<?php
+}
+else
+{
+?>
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
+<?php
+}
+?>
     <div class="secondary-slider-background"></div>
     <div class="slider-textblock-without-blogname">
         <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
@@ -8,3 +21,4 @@
         <!-- Blog name removed - not needed in this theme. -->
     </div>
 </div>
+
