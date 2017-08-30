2c32c104ed03192ad79ccd7e7b2b55f0700b5d3a
Dan Adams <danadams@Dans-MacBook-Pro-2.local>
2017-08-30 17:47:38 -0400

    giving day slider

---
+++ page-templates/partials/secondary_slider_givingday.php
@@ -0,0 +1,10 @@
+<!-- Child 1 secondary page slider -->
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192_givingday ">
+    <div class="secondary-slider-background"></div>
+    <div class="slider-textblock-without-blogname">
+        <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+            } ?></div>
+        <!-- Blog name removed - not needed in this theme. -->
+    </div>
+</div>
