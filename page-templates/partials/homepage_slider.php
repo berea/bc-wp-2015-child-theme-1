02d39cc0c028dd2cb1602d2bc44bfbbd5e786924
Dan Adams <adamsdan@facstaff-adamsdan.berea.edu>
2019-10-18 09:53:36 -0400

    Replacing hashtag

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -107,7 +107,7 @@ font-size:2em;
             <div class="hero-nav-item-videotext">
             	<h1 class="gb-c-hero-home__title gb-u-type-alpha" style="width:auto;  border-bottom: 0px !important;color: white !important; font-size: 3em; text-shadow: 0 0 10px #000;">A College Like No Other</h1>
                     <div class="gb-c-hero-home__intro gb-u-type-p--xlarge">
-						<p aria-hidden="true" style="letter-spacing: -0.001em;font-size:1.4em; margin-top: -20px; text-shadow: 0 0 10px #000;color: white !important; text-align:right !important;">Radical Since 1855</p>
+						<p aria-hidden="true" style="letter-spacing: -0.001em;font-size:1.4em; margin-top: -20px; text-shadow: 0 0 10px #000;color: white !important; text-align:right !important;">#ThisIsBerea</p>
 					</div>
                     
             </div>
