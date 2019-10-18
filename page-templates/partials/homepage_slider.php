c21ced1909271138c9964d96ad03d9b6a5e9ca5e
Dan Adams <adamsdan@facstaff-adamsdan.berea.edu>
2019-10-18 09:52:18 -0400

    Adding video to homepage

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -1,39 +1,121 @@
+<!-- Parent theme homepage slider -->
+
 <!-- This is the homepage slider -->
 
-<div class="homepage-slider homepage-slider-sizing-aspect-ratio ratio_1024x440">
-<!--	<div class="homepage-slider-background"></div>-->
+<style>
+.video-main {
+    width:100%;
+    left: 50%;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
 
-<div class="homepage-slider-background">
-    <?php if ( function_exists( 'soliloquy' ) ) { soliloquy( 'default', 'slug' ); } ?>
+.video-text-container{
+
+	
+}
+
+.hero-nav-videotext
+{
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    padding: 0 20px;
+    padding-top: 20px;
+    margin: 0 auto;
+    color: #FFFFFE;
+    font-size: 1.2em;
+    font-weight: bold;
+
+}
+
+
+.hero-nav-wrapper-videotext
+{
+    position:absolute;
+    top: 0px;
+    left: 0px;
+    width:100%;
+    height:100%;
+    background-color:rgba(34,34,34,0.2);
+
+}
+
+.hero-nav-item-videotext
+{
+    background-color: transparent;
+    position: absolute;
+    bottom: 15%;
+    right:5%;
+}
+
+.homepageslider-videotext
+{
+text-align: right;
+    color: #FFFFFE;
+    text-shadow: 0 0 10px #000;
+    padding-bottom: 0px;
+    border-bottom: none;
+    font-size: 4em;
+    line-height: 1.3em;
+}
+
+.homepageslider h1
+{
+font-size:2em;
+}
+
+</style>
+
+
+<div class="homepage-slider homepage-slider-sizing-aspect-ratio ratio_1024x440">
+
+
+<div class="homepage-slider-background" style="bottom: 0px !important; ">
+
+<div class="homepage-video">
+
+    
+      
+     <?php 
+
+//echo do_shortcode('[videojs_video url="http://d3fbrw1lypbzv6.cloudfront.net/bcCover-9.mp4" webm="http://d3fbrw1lypbzv6.cloudfront.net/bcCover-webmhd.webm"]'); 
+     //echo do_shortcode('[fvplayer src="rtmp://s3qtfh0q6d6acq.cloudfront.net/bcCover-9.mp4"]');  
+
+     ?> 
+     
+ 
+
+
+<video class="video-main" autoplay muted loop playsinline poster="placeholder.jpg" id="bgvideo" width="x" height="y">
+    <source src="http://d3fbrw1lypbzv6.cloudfront.net/bcCover-webmhd.webm" type="video/webm">
+    <source src="http://d3fbrw1lypbzv6.cloudfront.net/bcCover-9.mp4" type="video/mp4">
+</video>
+
+
+
+
 </div>
 
-    <div class="hero-nav-wrapper">
-        <div class="hero-nav">
-            <div class="hero-nav-item">
-                <a href="https://primary.berea.edu/admissions/apply/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apply.png" width="25" height="25" alt=""/>
-                    <span class="vertical-center nudge-right">APPLY</span>
-                </a>
+</div>
+
+    <div class="hero-nav-wrapper-videotext">
+        <div class="hero-nav-videotext">
+        	<div class="video-text-container">
+            <div class="hero-nav-item-videotext">
+            	<h1 class="gb-c-hero-home__title gb-u-type-alpha" style="width:auto;  border-bottom: 0px !important;color: white !important; font-size: 3em; text-shadow: 0 0 10px #000;">A College Like No Other</h1>
+                    <div class="gb-c-hero-home__intro gb-u-type-p--xlarge">
+						<p aria-hidden="true" style="letter-spacing: -0.001em;font-size:1.4em; margin-top: -20px; text-shadow: 0 0 10px #000;color: white !important; text-align:right !important;">Radical Since 1855</p>
+					</div>
+                    
             </div>
-            <div class="hero-nav-item">
-                <a href="https://primary.berea.edu/give-to-berea/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/give.png" width="25" height="25" alt=""/>
-                    <span class="vertical-center nudge-right">GIVE</span>
-                </a>
-            </div>
-            <div class="hero-nav-item">
-                <a href="https://primary.berea.edu/admissions/visit-campus/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/visit.png" width="25" height="25" alt=""/>
-                    <span class="vertical-center nudge-right">VISIT</span>
-                </a>
-            </div>
-            <div class="hero-nav-item">
-                <a href="https://www.berea.edu/campus/">
-                    <img class="vertical-center" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/explore.png" width="25" height="25" alt=""/>
-                    <span class="vertical-center nudge-right">EXPLORE</span>
-                </a>
-            </div>
+        </div>
         </div>
     </div>
 
+
+
 </div><!-- .homepage-slider -->
+
